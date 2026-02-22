c514002a43c151e17673aee097d1db0d8d41f562
deceeeeee <micha3l_dct@yahoo.com>
2026-02-23 02:34:02 +0700

    Edit booking statistic endpoint

--- src/features/bookings/bookings.api.ts
+++ src/features/bookings/bookings.api.ts
@@ -2,7 +2,7 @@ import axiosInstance from '../../services/axios';
 import type { BookingStatisticsResponse } from './bookings.types';
 
 export const fetchBookingStatistics = async (year: number): Promise<BookingStatisticsResponse> => {
-    const response = await axiosInstance.get<BookingStatisticsResponse>(`/admin/booking/statistic`, {
+    const response = await axiosInstance.get<BookingStatisticsResponse>(`/admin/booking`, {
         params: { year }
     });
     return response.data;
